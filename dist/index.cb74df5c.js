fe1069ffb550f90e9ffc1eb4ce623a5cf7a9314b
Greta <Greta1096@outlook.com>
2022-06-22 17:46:27 +0800

    dist 文件

---
+++ dist/index.cb74df5c.js
@@ -0,0 +1,75 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find('li.last');
+const x = localStorage.getItem('x');
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+    {
+        logo: "A",
+        url: "https://www.acfun.cn"
+    },
+    {
+        logo: "B",
+        url: "https://www.bilibili.com"
+    }
+];
+let isInputFocus = false;
+const simplifyUrl = (url)=>{
+    return url.replace('https://', '').replace('http://', '').replace('www.', '').replace(/\/.*/, '');
+};
+const render = ()=>{
+    $siteList.find('li:not(.last)').remove();
+    hashMap.forEach((node, index)=>{
+        const $li = $(`<li>
+          <div class="site">
+            <div class="logo">${node.logo}</div>
+            <div class="link">${simplifyUrl(node.url)}</div>
+            <div class = "close">
+              <svg class="icon">
+                <use xlink:href="#icon-close"></use>
+              </svg>
+            </div>
+          </div>
+        </li>`).insertBefore($lastLi);
+        $li.on('click', ()=>{
+            window.open(node.url);
+        });
+        $li.on('click', '.close', (e)=>{
+            e.stopPropagation();
+            hashMap.splice(index, 1);
+            render();
+        });
+    });
+};
+render();
+$('.searchForm > input').focus(()=>{
+    isInputFocus = true;
+    console.log(isInputFocus, "input");
+});
+$('.searchForm > input').blur(()=>{
+    isInputFocus = false;
+    console.log(isInputFocus, "input");
+});
+$('.addButton').on('click', ()=>{
+    let url = window.prompt('请问你要添加的网址是啥？');
+    if (url.indexOf('http') !== 0) url = 'https://' + url;
+    console.log(url);
+    hashMap.push({
+        logo: simplifyUrl(url)[0].toUpperCase(),
+        url: url
+    });
+    render();
+});
+//本地缓存
+window.onbeforeunload = ()=>{
+    const string = JSON.stringify(hashMap);
+    localStorage.setItem('x', string);
+};
+//键盘事件
+$(document).on('keypress', (e)=>{
+    if (isInputFocus) return;
+    const { key  } = e //const key = e.key
+    ;
+    for(let i = 0; i < hashMap.length; i++)if (hashMap[i].logo.toLowerCase() === key) window.open(hashMap[i].url);
+});
+
+//# sourceMappingURL=index.cb74df5c.js.map
